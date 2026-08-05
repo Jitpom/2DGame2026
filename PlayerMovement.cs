a6e1a412b0fea87ab67c272e31bc61a1dc9a2454
Jitpom <jittrapol@gmail.com>
2026-08-05 13:43:20 +0700

    Implement player sprite flipping based on movement direction
    Added localScale variable to handle player direction.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,11 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Animator anim;
+    private float localScale;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         speed = 5f;
         anim = GetComponent<Animator>();
+        localScale = gameObject.transform.localScale.x;
     }
 
     // Update is called once per frame
@@ -17,6 +19,16 @@ public class PlayerMovement : MonoBehaviour
         float  horizontalInput = Input.GetAxis("Horizontal"); 
         anim.SetFloat("RunningSpeed", Mathf.Abs(horizontalInput));
         
+        
+        if (horizontalInput < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-localScale, localScale, localScale);
+        }
+        else if (horizontalInput > 0)
+        {
+            gameObject.transform.localScale = new Vector3(localScale, localScale, localScale);
+        }
+
         gameObject.transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);   
     }
 }
