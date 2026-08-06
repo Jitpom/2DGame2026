80339b49a8d9e652ebff03296ece674516b552a3
Jitpom <jittrapol@gmail.com>
2026-08-06 13:23:30 +0700

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
